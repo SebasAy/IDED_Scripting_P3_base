bbb3b3d44ad2ca43e083c533eb12806367f50ff0
SebasAy <sebasaa2002@gmail.com>
2023-10-28 09:56:50 -0500

    Cambios puntos 3 y 4

--- Assets/Scripts/Refactored/RefactoredGameController.cs
+++ Assets/Scripts/Refactored/RefactoredGameController.cs
@@ -1,4 +1,68 @@
+using System.Xml;
+using UnityEngine;
+using UnityEngine.Events;
+
 public class RefactoredGameController : GameControllerBase
 {
-    public override PlayerControllerBase PlayerController => throw new System.NotImplementedException();
+    public static RefactoredGameController Instance { get; private set; }
+
+    [SerializeField]
+    private RefactoredPlayerController playerController;
+
+    // Eventos para notificar el fin del juego y los disparos de flechas.
+    public UnityEvent OnGameOver { get; private set; } = new UnityEvent();
+    public UnityEvent OnArrowShot { get; private set; } = new UnityEvent();
+
+    public override PlayerControllerBase PlayerController => playerController;
+
+    protected override void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        base.Awake();
+    }
+
+    private void Update()
+    {
+        // Lógica de actualización del juego
+    }
+
+    public override void ProcessShot(Vector3 aimPosition)
+    {
+        // Lógica para procesar el disparo
+        // Actualiza el marcador de puntaje, calcula el puntaje, y determina el Game Over.
+        UpdateScore(aimPosition);
+        CalculateScore(aimPosition);
+        CheckGameOver();
+    }
+
+    // Actualiza el marcador de puntaje.
+    private void UpdateScore(Vector3 aimPosition)
+    {
+        // Implementa la lógica de actualización del puntaje aquí.
+    }
+
+    // Calcula el puntaje del disparo.
+    private void CalculateScore(Vector3 shotPosition)
+    {
+        // Implementa la lógica de cálculo de puntaje aquí.
+    }
+
+    // Verifica si el juego ha terminado.
+    private bool CheckGameOver()
+    {
+        if (RemainingArrows <= 0)
+        {
+            OnGameOver.Invoke();
+            return true;
+        }
+
+        return false;
+    }
 }
--- Assets/Scripts/Refactored/RefactoredPlayerController.cs
+++ Assets/Scripts/Refactored/RefactoredPlayerController.cs
@@ -1,9 +1,47 @@
+using System.Windows.Input;
 using UnityEngine;
 
 public class RefactoredPlayerController : PlayerControllerBase
 {
+    public static RefactoredPlayerController Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     protected override void ProcessShot(Vector3 point)
     {
-        throw new System.NotImplementedException();
+        // Crea un comando y ejecútalo.
+        ICommand shotCommand = new ProcessShotCommand(point);
+        shotCommand.Execute();
     }
+
+    // Implementa la interfaz ICommand.
+    private class ProcessShotCommand : ICommand
+    {
+        private Vector3 aimPosition;
+
+        public ProcessShotCommand(Vector3 aimPosition)
+        {
+            this.aimPosition = aimPosition;
+        }
+
+        public void Execute()
+        {
+            RefactoredGameController.Instance.ProcessShot(aimPosition);
+            RefactoredGameController.Instance.OnArrowShot.Invoke();
+        }
+    }
+}
+public interface ICommand
+{
+    void Execute();
 }
